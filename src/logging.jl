3ae6ad31b030b0b2467dcc0143faf903045c2335
Andrew Dyami Cadwallader <andrew.cadwallader@gmail.com>
2017-10-02 15:44:05 -0700

    readded logging.jl

---
+++ src/logging.jl
@@ -0,0 +1,18 @@
+function logging(stream::Requests.ResponseStream, log_path::String)
+    while true
+        tick = pipe(stream)
+        open(log_path,"a") do f
+            write(f,Dates.format(now(), "yyyy-mm-ddTHH:MM:SS.sss"), " ")
+            write(f,tick["time"]," ")
+            write(f,tick["instrument"],"\n")
+        end
+    end
+end
+
+function log_test(timestamps::Array{Any,1})
+    sorted = [find(timestamps .== i)[1] for i in sort(timestamps)] #duplicate indices if duplicates in timestamps due to find()[1]
+    straight_line = [i for i in 1:length(test1)] #vector of indices
+    deltas = sorted - straight_line
+    running_tally = [sum(deltas[1:i]) for i in 1:length(deltas)]
+    StatsBase.countmap(deltas)
+end
