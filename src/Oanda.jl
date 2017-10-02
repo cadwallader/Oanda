7704f2a2fd04e8523d1dd581cde64c579be56158
Andrew Dyami Cadwallader <andrew.cadwallader@gmail.com>
2017-10-02 15:38:35 -0700

    added logging.jl

--- src/Oanda.jl
+++ src/Oanda.jl
@@ -1,8 +1,10 @@
 __precompile__()
 module Oanda
 using Requests, JSON
-export pricing, pipe
+export pricing, pipe, lineTest, logging
 include("init.jl")
 include("pricing.jl")
 include("pipe.jl")
+include("tests.jl")
+include("logging.jl")
 end
--- src/pipe.jl
+++ src/pipe.jl
@@ -3,13 +3,13 @@ function pipe(stream::Requests.ResponseStream)
         #yield()
     end
     tick = JSON.parse(readline(stream.buffer))
-    get(tick,"type","") == "PRICE" ? tick: pipe(stream)
+    get(tick,"type","") == "PRICE" ? flatten(tick): pipe(stream)
 end
 
 function flatten(tick::Dict{String,Any})
     Dict(
         "instrument" => tick["instrument"],
-        "time" => Dates.DateTime(tick["time"][1:end-7], Dates.DateFormat("y-m-dTH:M:S.s")),
+        "time" => tick["time"][1:end-7],
         "liquidityAsk" => tick["asks"][1]["liquidity"],
         "liquidityBid" => tick["bids"][1]["liquidity"],
         "closeoutAsk" => parse(Float64,tick["closeoutAsk"]),
