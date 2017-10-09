3f084716417ffc15d59d63087f6a6e0d25dfbdbf
Andrew Dyami Cadwallader <andrew.cadwallader@gmail.com>
2017-10-09 10:50:35 -0700

    added instruments.jl to get list of tradeable instruments

--- src/Oanda.jl
+++ src/Oanda.jl
@@ -1,10 +1,11 @@
 __precompile__()
 module Oanda
 using Requests, JSON, StatsBase
-export pricing, pipe, lineTest, logging, books
+export pricing, pipe, lineTest, logging, books, instruments
 include("init.jl")
 include("pricing.jl")
 include("pipe.jl")
 include("logging.jl")
 include("books.jl")
+inclued("instruments.jl")
 end
