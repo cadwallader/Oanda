ac3fc4a103c7a3ff5c986ca341f169c5ec2a0f5b
Andrew Dyami Cadwallader <andrew.cadwallader@gmail.com>
2017-10-31 20:03:08 -0700

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using Oanda
 using Base.Test
-
+@testset "pairs" begin
+    #
+    #
+end
 @testset "pricing" begin
-    @test typeof(pricing("USD_JPY")) == HttpCommon.Response
-    @test typeof(pricing("USD_JPY",true)) == Requests.ResponseStream{MbedTLS.SSLContext}
-    @test typeof(pricing(["USD_JPY"])) == HttpCommon.Response
-    @test typeof(pricing(["USD_JPY"],true)) == Requests.ResponseStream{MbedTLS.SSLContext}
+    @test typeof(price("USD_JPY")) == HttpCommon.Response
+    @test typeof(price_stream("USD_JPY",true)) == Requests.ResponseStream{MbedTLS.SSLContext}
+    @test typeof(price(["USD_JPY"])) == HttpCommon.Response
+    @test typeof(price_stream(["USD_JPY"],true)) == Requests.ResponseStream{MbedTLS.SSLContext}
 end
